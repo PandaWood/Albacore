9bdea94994225b75b99171582f765b6521d67e01
Peter van der Woude <pandawoude@gmail.com>
2017-02-19 16:10:45 +1100

    add msbuild finder

--- lib/albacore/config/msbuildconfig.rb
+++ lib/albacore/config/msbuildconfig.rb
@@ -22,9 +22,38 @@ module Configuration
     end
 
     def use(netversion)
-      # self.command = File.join(get_net_version(netversion), "MSBuild.exe")
-      self.command = "MSBuild.exe"    # above ends up using wrong version, so we'll fall back to relying on the correct one being in the path, for now
+	    msb = 'msbuild_not_found'
+      versions = find_msbuild_versions
+      if versions.any?
+        msb = versions[versions.keys.max]
+      end
+      self.command = msb
+    end
+
+		### imported from latest version of albacore https://github.com/Albacore/albacore.git
+	 	def find_msbuild_versions
+	    return nil unless ::Rake::Win32.windows?
+	    require 'win32/registry'
+	    retval = Hash.new
+	    begin
+	      Win32::Registry::HKEY_LOCAL_MACHINE.open('SOFTWARE\Microsoft\MSBuild\ToolsVersions') do |toolsVersion|
+	        toolsVersion.each_key do |key|
+	          begin
+	            version_key = toolsVersion.open(key)
+	            version = key.to_i
+	            msb = File.join(version_key['MSBuildToolsPath'],'msbuild.exe')
+	            retval[version] = msb
+	          rescue
+	            error "failed to open #{key}"
+	          end
+	        end
+	      end
+	    rescue
+	      error "failed to open HKLM\\SOFTWARE\\Microsoft\\MSBuild\\ToolsVersions"
+	    end
+	    retval
     end
   end
 end
+
 
